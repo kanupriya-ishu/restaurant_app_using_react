7ba9fc9d16889359a18e0383440661d9275529bd
kanupriya-ishu <kanupriya2512@gmail.com>
2022-01-16 12:26:07 +0530

    Redux Thunk

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem, CardBody, CardText, Modal, ModalBody, ModalHeader, Button, Row, Col, Label  } from "reactstrap";
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -179,13 +180,25 @@ class CommentForm extends Component{
 
 const DishDetail = (props) => {
 
-    const dish = props.dish
-    
-
-    if (dish == null) {
-        return (<div></div>);
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
     }
-
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null) 
     return (
         <div className="container">
             <div className="row">
